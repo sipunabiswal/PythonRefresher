c619f0c538397d8bef82a5e6216899718cb33500
sipuna.biswal <sipuna.biswal@gmail.com>
2026-01-04 16:57:05 +0530

    added inheritance sample

---
+++ oops/Zombie.py
@@ -0,0 +1,24 @@
+
+class Zombie:
+    def __init__(self, name, health, attack_power):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+    def attack(self):
+        return f"{self.name} attacks with power {self.attack_power}!"
+    
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            return f"{self.name} has been defeated!"
+        else:
+            return f"{self.name} takes {damage} damage and has {self.health} health left."
+        
+    def get_type_of_enemy(self):
+        return "Zombie"
+    
+    def talk(self):
+        return f"{self.name} is a zombie."
+    
+    def walk_forward(self):
+        return f"{self.name} shambles forward."
